134b027d2494b5e9c205d2e0380722c3f34fc764
Bala Shanmugam <35741731+bashan-git@users.noreply.github.com>
2023-04-28 13:25:54 -0700

    Commiting Event Code

--- EmailTelemetry/EmailTelemetryProcessingService/EmailTelemetryProcessingService/EmailTelemetryEventHubTrigger.cs
+++ EmailTelemetry/EmailTelemetryProcessingService/EmailTelemetryProcessingService/EmailTelemetryEventHubTrigger.cs
@@ -1,19 +1,41 @@
 using Azure.Messaging.EventHubs;
+using EmailTelemetryProcessingService.Models;
+using Microsoft.Azure.Amqp.Framing;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Serialization.HybridRow.Schemas;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace EmailTelemetryProcessingService
 {
-    public static class EmailTelemetryEventHubTrigger
+    public class EmailTelemetryEventHubTrigger
     {
+        private readonly CosmosClient _cosmosClient;
+        private readonly Database _database;
+        private readonly Microsoft.Azure.Cosmos.Container _container;
+        private string _databaseName;
+        private string _containerName;
+        private string _partitionKey;
 
+        public EmailTelemetryEventHubTrigger(CosmosClient cosmosClient, IConfiguration configuration)
+        {
+            _cosmosClient = new CosmosClient(configuration["COSMOSDB_CONNECTION_STRING"]);
+            _partitionKey = configuration["CampaignId"];
+            _databaseName = configuration["DatabaseName"];
+            _containerName = configuration["CollectionName"];
+            _container= _cosmosClient.GetContainer(_databaseName, _containerName);
+        }
 
         [FunctionName("EmailTelemetryEventHubTrigger")]
-        public static async Task Run([EventHubTrigger("email-telemetry-eventhub", Connection = "EVENT_HUB_CONNECTION_STRING")] EventData[] events, ILogger log)
+        public async Task Run([EventHubTrigger("email-telemetry-eventhub", Connection = "EVENT_HUB_CONNECTION_STRING")] EventData[] events, ILogger log)
         {
             var exceptions = new List<Exception>();
 
@@ -21,14 +43,34 @@ namespace EmailTelemetryProcessingService
             {
                 try
                 {
-                    // Replace these two lines with your processing logic.
-                    log.LogInformation($"C# Event Hub trigger function processed a message: {eventData.EventBody}");
+                    EventRecord eventRecord = JsonConvert.DeserializeObject<EventRecord>(Encoding.UTF8.GetString(eventData.EventBody));
+                    foreach (Record record in eventRecord.records)
+                    {
+                        // Mail - Delivery Status Tracking
+                        if (record.operationName.Equals("DeliveryStatusUpdate"))
+                        {
+                            log.LogInformation($"ACS Email - Correlation ID : {record.correlationId} - DeliveryStatus - {record?.properties.DeliveryStatus}");
+                                                       
+                            try
+                            {
+                                var response = await _container.ReadItemAsync<EmailListDto>(record.properties.RecipientId, new Microsoft.Azure.Cosmos.PartitionKey(_partitionKey));
+                                var oldItem = response.Resource;
+
+                                oldItem.Status = "Completed";
+
+                                await _container.ReplaceItemAsync(oldItem, record.properties.RecipientId, new Microsoft.Azure.Cosmos.PartitionKey(_partitionKey));
+                            }
+                            catch (CosmosException ex) when (ex.StatusCode == System.Net.HttpStatusCode.NotFound)
+                            {
+                                throw new Exception($"Item {record.properties.RecipientId} not found for this Campaign {_partitionKey}");
+                            }
+                        }
+                        
+                    }
                     await Task.Yield();
                 }
                 catch (Exception e)
                 {
-                    // We need to keep processing the rest of the batch - capture this exception and continue.
-                    // Also, consider capturing details of the message that failed processing so it can be processed again later.
                     exceptions.Add(e);
                 }
             }
---
+++ EmailTelemetry/EmailTelemetryProcessingService/EmailTelemetryProcessingService/Models/EventRecord.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailTelemetryProcessingService.Models
+{
+    public class EventRecord
+    {
+        public List<Record> records { get; set; }
+    }
+}
