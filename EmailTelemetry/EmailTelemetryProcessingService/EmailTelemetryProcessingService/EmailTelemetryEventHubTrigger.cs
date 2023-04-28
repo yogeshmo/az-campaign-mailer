747f6a4ff6e606452ab62d9969fb5294aa1bd638
Avin Pattath <avinp@microsoft.com>
2023-04-28 11:52:22 -0700

    ADd email telemetry processing service function

---
+++ EmailTelemetry/EmailTelemetryProcessingService/EmailTelemetryProcessingService/EmailTelemetryEventHubTrigger.cs
@@ -0,0 +1,45 @@
+using Azure.Messaging.EventHubs;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmailTelemetryProcessingService
+{
+    public static class EmailTelemetryEventHubTrigger
+    {
+
+
+        [FunctionName("EmailTelemetryEventHubTrigger")]
+        public static async Task Run([EventHubTrigger("eventhub-email-telemetry", Connection = "EVENT_HUB_CONNECTION_STRING")] EventData[] events, ILogger log)
+        {
+            var exceptions = new List<Exception>();
+
+            foreach (EventData eventData in events)
+            {
+                try
+                {
+                    // Replace these two lines with your processing logic.
+                    log.LogInformation($"C# Event Hub trigger function processed a message: {eventData.EventBody}");
+                    await Task.Yield();
+                }
+                catch (Exception e)
+                {
+                    // We need to keep processing the rest of the batch - capture this exception and continue.
+                    // Also, consider capturing details of the message that failed processing so it can be processed again later.
+                    exceptions.Add(e);
+                }
+            }
+
+            // Once processing of the batch is complete, if any messages in the batch failed processing throw an exception so that there is a record of the failure.
+
+            if (exceptions.Count > 1)
+                throw new AggregateException(exceptions);
+
+            if (exceptions.Count == 1)
+                throw exceptions.Single();
+        }
+    }
+}
