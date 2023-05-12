9d48d84f01b79ef59ca21fd569f91518a4134ce8
Rajat Rastogi <rajatras@microsoft.com>
2023-05-12 12:26:45 -0700

    Add event grid processor

---
+++ EventGridTest/EmailEventData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EventGridTest
+{
+    internal class EmailEventData
+    {
+        public string Sender { get; set; }
+        public string Recipient { get; set; }
+        public string MessageId { get; set; }
+        public string Status { get; set; }
+        public DateTimeOffset DeliveryAttemptTimestamp { get; set; }
+
+        public override string ToString()
+        {
+            return $"Sender={Sender}, Recipient={Recipient}, MessageId={MessageId}, Status={Status}, DeliveryAttemptTimestamp={DeliveryAttemptTimestamp}";
+        }
+    }
+}
